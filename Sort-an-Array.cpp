f2bb222407ec61faecedcbf9f0cbd70ebdf35b02
Eeveee25 <123829607+Eeveee25@users.noreply.github.com>
2025-06-09 16:21:53 +0530

    [912] [Time Beats: 73.71%] [Memory Beats: 92.68%] - LeetPush

---
+++ Sort-an-Array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void heapify(vector<int>& nums, int n, int i) {
+        while (true) {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && nums[left] > nums[largest]) largest = left;
+            if (right < n && nums[right] > nums[largest]) largest = right;
+
+            if (largest == i) break;
+
+            swap(nums[i], nums[largest]);
+            i = largest;  // move down to the swapped child
+        }
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+
+        // Build max heap
+        for (int i = n / 2 - 1; i >= 0; --i)
+            heapify(nums, n, i);
+
+        // Extract elements from heap
+        for (int i = n - 1; i > 0; --i) {
+            swap(nums[0], nums[i]);      // move current root to end
+            heapify(nums, i, 0);         // heapify reduced heap
+        }
+
+        return nums;
+    }
+};
