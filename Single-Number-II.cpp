73b2fd4ff105fa806b3bacebaddd17b0a86eea91
Eeveee25 <123829607+Eeveee25@users.noreply.github.com>
2025-06-10 17:03:46 +0530

    [137] [Time Beats: 100.00%] [Memory Beats: 65.47%] - LeetPush

--- Single-Number-II.cpp
+++ Single-Number-II.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result = 0;
+        int ones = 0, twos = 0;
 
-        // Go through each bit position
-        for (int i = 0; i < 32; i++) {
-            int bitCount = 0;
-
-            for (int num : nums) {
-                // Check if the i-th bit is set in num
-                if ((num >> i) & 1) {
-                    bitCount++;
-                }
-            }
-
-            // If the bitCount is not divisible by 3, that bit is set in the single number
-            if (bitCount % 3 != 0) {
-                result |= (1 << i);
-            }
+        for (int num : nums) {
+            // First appearance: add it to ones
+            // Second appearance: remove it from ones and add it to twos
+            // Third appearance: remove it from twos
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
         }
 
-        return result;
+        return ones;
     }
 };
