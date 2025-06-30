d8bb5c39926af22da9cfb76d96474bc7590f4300
Srishti <123829607+Eeveee25@users.noreply.github.com>
2025-06-30 14:29:16 +0530

    Update #2149 Rearrange-Array-Elements-by-Sign.cpp

--- #2149 Rearrange-Array-Elements-by-Sign.cpp
+++ #2149 Rearrange-Array-Elements-by-Sign.cpp
@@ -19,3 +19,29 @@ public:
         return result;
     }
 };
+
+//two pointer approch 
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> pos;
+        vector<int> neg;
+
+        // Step 1: Separate positive and negative numbers
+        for (int num : nums) {
+            if (num > 0)
+                pos.push_back(num);
+            else
+                neg.push_back(num);
+        }
+
+        // Step 2: Interleave pos and neg alternately
+        vector<int> result;
+        for (int i = 0; i < pos.size(); i++) {
+            result.push_back(pos[i]);   // Add positive
+            result.push_back(neg[i]);   // Add negative
+        }
+
+        return result;
+    }
+};
