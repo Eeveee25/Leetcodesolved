e40613eed79ee43283ed7e61d6373a6df2a0e440
Srishti <123829607+Eeveee25@users.noreply.github.com>
2025-06-30 13:06:55 +0530

    [2149] [Time Beats: 100.00%] [Memory Beats: 99.64%] - LeetPush

--- Rearrange-Array-Elements-by-Sign.cpp
+++ Rearrange-Array-Elements-by-Sign.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> pos;
-        vector<int> neg;
+        int n = nums.size();
+        vector<int> result(n);  // preallocate result vector
+        int posIndex = 0;
+        int negIndex = 1;
 
-        // Step 1: Separate positive and negative numbers
         for (int num : nums) {
-            if (num > 0)
-                pos.push_back(num);
-            else
-                neg.push_back(num);
-        }
-
-        // Step 2: Interleave pos and neg alternately
-        vector<int> result;
-        for (int i = 0; i < pos.size(); i++) {
-            result.push_back(pos[i]);   // Add positive
-            result.push_back(neg[i]);   // Add negative
+            if (num > 0) {
+                result[posIndex] = num;
+                posIndex += 2;
+            } else {
+                result[negIndex] = num;
+                negIndex += 2;
+            }
         }
 
         return result;
