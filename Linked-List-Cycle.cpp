4cf7dab73bff439adc7e95f49c15e24e8b567c3d
Srishti <123829607+Eeveee25@users.noreply.github.com>
2025-07-15 16:23:51 +0530

    [141] [Time Beats: 81.01%] [Memory Beats: 53.31%] - LeetPush

---
+++ Linked-List-Cycle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;           // 1 step
+            fast = fast->next->next;     // 2 steps
+
+            if (slow == fast) {
+                return true;             // Cycle detected
+            }
+        }
+
+        return false; // No cycle
+    }
+};
